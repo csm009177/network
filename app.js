62ce4408c5f97987752c3ddbb09792a7d0b708e2
csm <csm009177@gmail.com>
2023-10-18 20:33:47 +0900

    make readFile

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 // import hyper text
 const http = require('http');
+const contentType = require('./mod/contentType');
 
 
 // createServer 
@@ -29,6 +30,15 @@ let server = http.createServer((req,res)=> {
       console.log(`form 입력으로부터 받은 데이터 확인 ->`, parsedBody);
       console.log(`form 입력으로부터 받은 데이터 확인 ->`, username);
       console.log(`form 입력으로부터 받은 데이터 확인 ->`, password);
+
+    })
+    fs.readFile('./doc/index.html', (err, data)=> {
+      if(err) {
+        console.log(err);
+      } else {
+        res.writeHead(200, contentType)
+        res.end(data)
+      }
     })
   }
 });
