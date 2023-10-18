2ffe62b7d18e646b73cbc34fb26875d220dc43bb
csm <csm009177@gmail.com>
2023-10-18 20:28:39 +0900

    data refine

--- app.js
+++ app.js
@@ -5,15 +5,35 @@ const http = require('http');
 
 
 // createServer 
-http.createServer((req,res)=> {
+let server = http.createServer((req,res)=> {
   if(req.method === 'GET' && req.url === '/'){
     fs.readFile('./doc/index.html', (err, data)=> {
       if(err) {
-        console.log(err)
+        console.log(err);
       } else {
-        res.writeHead(200, './mod/contentType.js')
-        res.end(data)
-      }
-    })  
-  } 
+        res.writeHead(200, './mod/contentType.js');
+        res.end(data);
+      };
+    });  
+  } else if (req.method=== 'POST' && req.url === '/doc/login'){
+    let body ='';
+    
+    req.on('data', (chunk) =>{
+      body = body + chunk.toString();
+    })
+    req.on('end', () => {
+      const parsedBody = queryString.parse(body);
+      const {username, password} = parsedBody;
+
+      // check 
+      console.log(`form 입력으로부터 받은 데이터 확인 ->`, parsedBody);
+      console.log(`form 입력으로부터 받은 데이터 확인 ->`, username);
+      console.log(`form 입력으로부터 받은 데이터 확인 ->`, password);
+    })
+  }
+});
+
+let port = 2345
+server.listen(port, () => {
+  console.log(`http://localhost:${port}`);
 })
