6a17d03f4ac90c5633efbc4fe302307954005360
csm <csm009177@gmail.com>
2023-10-19 10:16:36 +0900

    make link in terminal

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((req, res)=> {
+let server = http.createServer((req, res)=> {
   if(req.method === 'GET' && req.url==='/') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end('hello')
   }
-}).listen(8080);
+})
+let port = 8080;
+server.listen(port, () => {
+console.log(`아래 주소를 누르세요
+http://localhost:${port}`)
+});
