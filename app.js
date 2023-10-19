a083cb6091c37644a1745f66d8611a54b0c5bac8
csm <csm009177@gmail.com>
2023-10-19 20:22:48 +0900

    put param in docMaker

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const http = require('http');
+const docMaker = require('./mod/docMaker');
 
 
 let serv = http.createServer((req, res)=> {
   if(req.method==='GET'&&req.url==='/'){
     console.log(req.method);
     console.log(req.url);
-    res.end('hello');
+    res.end(docMaker());
   } else if (req.method==='POST'&&req.url==='/'){
     console.log(req.method);
     console.log(req.url);
   }
 });
 
-let port = 2517
+let port = 2517;
 serv.listen(port, ()=>{
   console.log(`http://localhost:${port}`)
 })
