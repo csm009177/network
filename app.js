07b9f1a910314d1286ace932ce5ce0fc74083483
csm <csm009177@gmail.com>
2023-10-19 17:35:13 +0900

    make new html for docmaker module

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const http = require('http');
 const fs = require('fs');
+let contentType =require('./mod/contentType.js')
 
 let server = http.createServer((req,res)=> {
   if(req.method==='GET'&& req.url==='/'){
-    res.writeHead()
-    res.end()
+    res.writeHead(200, contentType)
+    res.end('hello')
   }
+  if(req.method==='POST'&& req.url==='/'){
+    
+  }
+})
+
+let port = 2315
+server.listen(port, ()=> {
+  console.log(`
+now server is running
+please click this link with Clt
+http://localhost:${port}
+  `
+  )
 })
