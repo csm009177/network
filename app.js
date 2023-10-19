7eb7709d44784d7d9a027006b215304d6986175f
csm <csm009177@gmail.com>
2023-10-19 20:52:51 +0900

    apply style.css to app.js

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const http = require('http');
 const docMaker = require('./mod/docMaker');
+const contentType = require('./mod/contentType.js');
+
+let maindoc = docMaker('main', 'test');
 
 
 let serv = http.createServer((req, res)=> {
   if(req.method==='GET'&&req.url==='/'){
     console.log(req.method);
     console.log(req.url);
-    res.end(docMaker());
-  } else if (req.method==='POST'&&req.url==='/'){
+    res.writeHead(200, contentType);
+    res.end(maindoc);
+  } else if(req.url==='/doc/style.css'){
+    res.writeHead(200, contentType);
+    res.end()
+  }
+  
+  
+  
+  else if (req.method==='POST'&&req.url==='/'){
     console.log(req.method);
     console.log(req.url);
   }
--- mod/docMaker.js
+++ mod/docMaker.js
@@ -4,6 +4,7 @@ function docMaker(title, contents){
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/doc/style.css">
     <title>${title}</title>
   </head>
   <body>
