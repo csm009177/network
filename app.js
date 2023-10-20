7a82bcffa1535411147487b72d19f5ec79a28cad
csm <csm009177@gmail.com>
2023-10-20 15:33:38 +0900

    make check method and url

--- app.js
+++ app.js
@@ -1,58 +1,59 @@
 // import big module
-const http = require('http');
-const fs = require('fs');
-const queryString = require('querystring');
-const docMaker = require('./mod/docMaker');
-queryString.parse()
+const http = require("http");
+const fs = require("fs");
+const queryString = require("querystring");
+const docMaker = require("./mod/docMaker");
+queryString.parse();
 
 // import contentType
-let cssContent = require('./mod/cssContent.js');
-let htmlContent = require('./mod/htmlContent.js');
+let cssContent = require("./mod/cssContent.js");
+let htmlContent = require("./mod/htmlContent.js");
 
-//make doc 
-let maindoc = docMaker('main', 'test');
+//make doc
+let maindoc = docMaker("main", "test");
 // fnc check method and url
 
-// serv 
-let serv = http.createServer((req, res)=> {
-  if(req.method==='GET'&&req.url==='/'){
+
+console.log(`data type : ${typeof(data)}`);
+
+
+// serv
+let serv = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
     //check method and url
     console.log(`
-    method check:${req.method}
-    url check:${req.url}`);
+    method check : ${req.method}
+    url check : ${req.url}`);
+    
     res.writeHead(200, htmlContent);
     res.end(maindoc);
-
-  } else if(req.url==='/doc/style.css'){
+  } else if (req.url === "/doc/style.css") {
     //check method and url
     console.log(`
-    method check:${req.method}
-    url check:${req.url}`);
-    
-    fs.readFile('./doc/style.css', 'utf8', (err,data)=> {
-      if(err){
-        console.log(`
-        readFile err check${err}`);
-      } 
-        else {
+    method check : ${req.method} 
+    url check : ${req.url}`);
+
+    fs.readFile("./doc/style.css", "utf8", (err, data) => {
+      if (err) { console.log(`readFile err check : ${err}`);
+      } else {
         res.writeHead(200, cssContent);
         res.end(data);
         // check data type
         console.log(`
-        type fo data : ${typeof(data)}`);
-        }
-      })
-    } else if (req.method==='POST'&&req.url==='/'){
-      //check method and url
-      console.log(`
-      method check:${req.method}
-      url check:${req.url}`);
+        type fo data : ${typeof data}`);
       }
+    });
+  } else if (req.method === "POST" && req.url === "/") {
+    //check method and url
+    console.log(`
+    method check : ${req.method}
+    url check : ${req.url}`);
+    
+  }
 });
 
-
-// serv.listen 
+// serv.listen
 let port = 2517;
-serv.listen(port, ()=>{
-  console.log(`http://localhost:${port}`)
-})
+serv.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
