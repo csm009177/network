52dd4a00284cc4047a6fb262261ca1a434bbb361
csm <csm009177@gmail.com>
2023-10-20 15:42:49 +0900

    merge branch

--- app.js
+++ app.js
@@ -36,51 +36,24 @@ let serv = http.createServer((req, res) => {
     fs.readFile("./doc/style.css", "utf8", (err, data) => {
       if (err) { console.log(`readFile err check : ${err}`);
       } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data)
-      };
+        res.writeHead(200, cssContent);
+        res.end(data);
+        // check data type
+        console.log(`
+        type fo data : ${typeof data}`);
+      }
     });
-  } else if (req.method === 'POST' && req.url === '/doc/login.html'){
-
-    let body = '';
+  } else if (req.method === "POST" && req.url === "/") {
+    //check method and url
+    console.log(`
+    method check : ${req.method}
+    url check : ${req.url}`);
     
-    req.on('data', (chunk) => {
-      body = body + chunk.toString();
-    });
-
-    req.on('end', () => { 
-      const parsedBody = queryString.parse(body);
-      const {username, password} = parsedBody;
-      // 비구조화할당 : value 바로 접근하는 방식
-      // check 
-      console.log(`form 입력으로부터 받은 데이터 확인 ->`, parsedBody);
-      console.log(`form 입력으로부터 받은 데이터 확인 ->`, username);
-      console.log(`form 입력으로부터 받은 데이터 확인 ->`, password);
-
-    });
-
-    fs.readFile(paths2, 'utf8', (err, data) => {
-      if(err) {
-        console.log(err)
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data)
-      }
-    })
-    fs.readFile(style1, 'utf8', (err, data) => {
-      if(err) {
-        console.log(err)
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data)
-      }
-    })
   }
 });
 
-
-let port = 2323;
-server.listen(port, () => {
-console.log(`아래 주소를 누르세요
-http://localhost:${port}`)
+// serv.listen
+let port = 2517;
+serv.listen(port, () => {
+  console.log(`http://localhost:${port}`);
 });
