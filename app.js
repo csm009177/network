7bdec6be456cb555097fb8a082a186a30f7b8afb
csm <csm009177@gmail.com>
2023-10-20 09:19:17 +0900

    try to check by methodAndurlCheck

--- app.js
+++ app.js
@@ -11,33 +11,44 @@ let htmlContent = require('./mod/htmlContent.js');
 //make doc 
 let maindoc = docMaker('main', 'test');
 
+// fnc check method and url
 
 // serv 
 let serv = http.createServer((req, res)=> {
   
   if(req.method==='GET'&&req.url==='/'){
-    console.log(req.method);
-    console.log(req.url);
+    methodAndurlCheck()
+    console.log(`
+    method check:${req.method}
+    url check:${req.url}`);
+
     res.writeHead(200, htmlContent);
     res.end(maindoc);
   } else if(req.url==='/doc/style.css'){
-    console.log(req.method);
-    console.log(req.url);
+    console.log(`
+    method check:${req.method}
+    url check:${req.url}`)
     
     fs.readFile('./doc/style.css', (err,data)=> {
       if(err){
-        console.log(err);
+        console.log(`
+    readFile err check${err}`);
+
       } else {
         res.writeHead(200, cssContent);
         res.end(data);
-        
+
+        console.log(`
+    type fo data : ${typeof(data)}`);
+
         }
       })
     } 
     
   else if (req.method==='POST'&&req.url==='/'){
-    console.log(req.method);
-    console.log(req.url);
+    console.log(`
+    method check:${req.method}
+    url check:${req.url}`);
   }
 });
 
