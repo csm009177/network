6920578427bcd62be1bd9ec3b93f340ca691119f
csm <csm009177@gmail.com>
2023-10-20 14:53:18 +0900

    fix line

--- app.js
+++ app.js
@@ -16,39 +16,38 @@ let maindoc = docMaker('main', 'test');
 // serv 
 let serv = http.createServer((req, res)=> {
   if(req.method==='GET'&&req.url==='/'){
+    //check method and url
     console.log(`
     method check:${req.method}
     url check:${req.url}`);
-
-    
     res.writeHead(200, htmlContent);
     res.end(maindoc);
+
   } else if(req.url==='/doc/style.css'){
+    //check method and url
     console.log(`
     method check:${req.method}
-    url check:${req.url}`)
+    url check:${req.url}`);
     
     fs.readFile('./doc/style.css', 'utf8', (err,data)=> {
       if(err){
         console.log(`
-    readFile err check${err}`);
-
-      } else {
+        readFile err check${err}`);
+      } 
+        else {
         res.writeHead(200, cssContent);
         res.end(data);
-
+        // check data type
         console.log(`
-    type fo data : ${typeof(data)}`);
-
+        type fo data : ${typeof(data)}`);
         }
       })
-    } 
-    
-  else if (req.method==='POST'&&req.url==='/'){
-    console.log(`
-    method check:${req.method}
-    url check:${req.url}`);
-  }
+    } else if (req.method==='POST'&&req.url==='/'){
+      //check method and url
+      console.log(`
+      method check:${req.method}
+      url check:${req.url}`);
+      }
 });
 
 
